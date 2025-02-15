8c7ddd9704a41979843a3fcaad4995903ce55151
Obito-git <antonmyroshnychenko@gmail.com>
2025-02-15 19:29:15 +0100

    fix rendered inline code block css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,34 @@ export default {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         serif: ["Lora", ...defaultTheme.fontFamily.serif],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "code::before": { content: '""' },
+            "code::after": { content: '""' },
+            "code": {
+              fontWeight: "normal",
+              backgroundColor: `${theme("colors.gray.100")} !important`,
+              padding: "0.2em 0.4em",
+              borderRadius: theme("borderRadius.sm"),
+              color: theme("colors.gray.800"),
+            },
+          },
+        },
+        dark: {
+          css: {
+            "code::before": { content: '""' },
+            "code::after": { content: '""' },
+            "code": {
+              fontWeight: "normal",
+              backgroundColor: `${theme("colors.gray.800")} !important`,
+              padding: "0.2em 0.4em",
+              borderRadius: theme("borderRadius.sm"),
+              color: theme("colors.gray.100"),
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [require("@tailwindcss/typography")],
