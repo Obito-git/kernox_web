bf61ee054ca5fcd530efd9f17eaea75581090976
Obito-git <antonmyroshnychenko@gmail.com>
2025-02-16 17:05:14 +0100

    final solution to fix code blocks and add copy button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
   ],
@@ -12,30 +12,24 @@ export default {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         serif: ["Lora", ...defaultTheme.fontFamily.serif],
       },
-      typography: (theme) => ({
+      typography: () => ({
         DEFAULT: {
           css: {
             "code::before": { content: '""' },
             "code::after": { content: '""' },
-            "code": {
-              fontWeight: "normal",
-              backgroundColor: `${theme("colors.gray.100")} !important`,
-              padding: "0.2em 0.4em",
-              borderRadius: theme("borderRadius.sm"),
-              color: theme("colors.gray.800"),
+            "code:not(pre code)": {
+              backgroundColor: "#c9c7c79c !important",
+              color: "#1f2937 !important",
             },
           },
         },
-        dark: {
+        invert: {
           css: {
             "code::before": { content: '""' },
             "code::after": { content: '""' },
-            "code": {
-              fontWeight: "normal",
-              backgroundColor: `${theme("colors.gray.800")} !important`,
-              padding: "0.2em 0.4em",
-              borderRadius: theme("borderRadius.sm"),
-              color: theme("colors.gray.100"),
+            "code:not(pre code)": {
+              backgroundColor: "#636a7554 !important",
+              color: "#f5f5f5 !important",
             },
           },
         },
